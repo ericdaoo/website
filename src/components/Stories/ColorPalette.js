f105311cc705bb026aa47d5e05a428ecbff01ea6
ericdaoo <53070250+ericdaoo@users.noreply.github.com>
2022-03-05 23:10:31 -0500

    More refinement to story page's flexbox

--- src/components/Stories/ColorPalette.js
+++ src/components/Stories/ColorPalette.js
@@ -10,9 +10,9 @@ const ColorPalette = (props) => {
     const elementRef = useRef();
     const elementRef2 = useRef();
 
-    // useEffect(() => {
-    //     initViz()
-    // }, [])
+    useEffect(() => {
+        initViz()
+    }, [])
 
     const options = {
         width: "800px",
@@ -24,7 +24,7 @@ const ColorPalette = (props) => {
     const initViz = () => {
         const vizUrl = "https://public.tableau.com/views/NYCIncomeDisparity/NYCIncomeDisparityMap?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 
-    // const initViz2 = () => {
+        // const initViz2 = () => {
         const vizUrl2 = "https://public.tableau.com/views/TaipeiIncomeDisparity/TaipeiIncomeDisparityMap?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
 
 
@@ -44,7 +44,7 @@ const ColorPalette = (props) => {
             </p>
             <div className="overlay-container">
 
-                <div className="overlay-ref" style={{animation: "color-fade-2a 5s infinite alternate"}}>
+                <div className="overlay-ref" style={{ animation: "color-fade-2a 5s infinite alternate" }}>
                     <p className="ref-title">References</p>
 
                     <p className="ref-text">[1] United States Census Burueu, Median Income in the Past 12 Months (In 2019 Inflated-Adjusted Dollars</p>
@@ -52,39 +52,27 @@ const ColorPalette = (props) => {
 
                     <p></p>
 
-                    <p className="ref-text"> [2] United States Census Burueu, Median Income in the Past 12 Months (In 2019 Inflated-Adjusted Dollars </p> 
+                    <p className="ref-text"> [2] United States Census Burueu, Median Income in the Past 12 Months (In 2019 Inflated-Adjusted Dollars </p>
                     <a className="ref-text" href="https://www.census.gov/cgi-bin/geo/shapefiles/index.php?year=2020&layergroup=ZIP+Code+Tabulation+Areas" target="_blank">Link</a>
 
-                    
 
-                    
+
+
                 </div>
-        
-                <h2 className="overlay-text" style={{animation: "color-fade-2a 2s infinite alternate"}}>Color Palette</h2>
-                <h4 className="overlay-text">"I long had an unquestioned assumption that large cities naturally equate to large disparities. It wasn’t until I lived in Taipei did I learn that this is a myth that we merely acceptted as a society in the US."</h4>
-                {/* <div ref={elementRef} className="overlay-visual"></div>
 
-                <div ref={elementRef2} className="overlay-visual"></div> */}
+                <h2 className="overlay-text" style={{ animation: "color-fade-2a 2s infinite alternate" }}>Color Palette</h2>
+                <div className="overlay-text">
+                    <h4 className="overlay-quote">"I long had an unquestioned assumption that large cities naturally equate to large disparities. It wasn’t until I lived in Taipei did I learn that this is a myth that we merely acceptted as a society in the US."</h4> </div>
+                
+                <div ref={elementRef} className="overlay-visual"></div>
+                <div ref={elementRef2} className="overlay-visual"></div>
+
                 <p className="overlay-text">CANDIDE
 
-                    In a castle of Westphalia, belonging to the Baron of Thunder-ten-Tronckh, lived a youth, whom nature had endowed with the most gentle manners. His countenance was a true picture of his soul. He combined a true judgment with simplicity of spirit, which was the reason, I apprehend, of his being called Candide. The old servants of the family suspected him to have been the son of the Baron's sister, by a good, honest gentleman of the neighborhood, whom that young lady would never marry because he had been able to prove only seventy-one quarterings, the rest of his genealogical tree having been lost through the injuries of time.
-
+                    In a castle of Westphalia, belonging to the Baron of Thunder-ten-Tronckh, lived a youth, whom nature had endowed with the most gentle manners. His countenance was a true picture of his soul. He combined a true judgment with simplicity of spirit, whichå was the reason, I apprehend, of his being called Candide. The old servants of the family suspected him to have been the son of the Baron's sister, by a good, honest gentleman of the neighborhood, whom that young lady would never marry because he had been able to prove only seventy-one quarterings, the rest of his genealogical tree having been lost through the injuries of time.
+                </p>
+                <p className="overlay-text">
                     The Baron was one of the most powerful lords in Westphalia, for his castle had not only a gate, but windows. His great hall, even, was[Pg 2] hung with tapestry. All the dogs of his farm-yards formed a pack of hounds at need; his grooms were his huntsmen; and the curate of the village was his grand almoner. They called him "My Lord," and laughed at all his stories.
-
-                    The Baron's lady weighed about three hundred and fifty pounds, and was therefore a person of great consideration, and she did the honours of the house with a dignity that commanded still greater respect. Her daughter Cunegonde was seventeen years of age, fresh-coloured, comely, plump, and desirable. The Baron's son seemed to be in every respect worthy of his father. The Preceptor Pangloss[1] was the oracle of the family, and little Candide heard his lessons with all the good faith of his age and character.
-
-                    Pangloss was professor of metaphysico-theologico-cosmolo-nigology. He proved admirably that there is no effect without a cause, and that, in this best of all possible worlds, the Baron's castle was the most magnificent of castles, and his lady the best of all possible Baronesses.
-
-                    "It is demonstrable," said he, "that things cannot be otherwise than as they are; for all being created for an end, all is necessarily for the best end. Observe, that the nose has been formed to bear spectacles—thus we have spectacles. Legs are visibly designed for stockings[Pg 3]—and we have stockings. Stones were made to be hewn, and to construct castles—therefore my lord has a magnificent castle; for the greatest baron in the province ought to be the best lodged. Pigs were made to be eaten—therefore we eat pork all the year round. Consequently they who assert that all is well have said a foolish thing, they should have said all is for the best."
-
-                    Candide listened attentively and believed innocently; for he thought Miss Cunegonde extremely beautiful, though he never had the courage to tell her so. He concluded that after the happiness of being born of Baron of Thunder-ten-Tronckh, the second degree of happiness was to be Miss Cunegonde, the third that of seeing her every day, and the fourth that of hearing Master Pangloss, the greatest philosopher of the whole province, and consequently of the whole world.
-
-                    One day Cunegonde, while walking near the castle, in a little wood which they called a park, saw between the bushes, Dr. Pangloss giving a lesson in experimental natural philosophy to her mother's chamber-maid, a little brown wench, very pretty and very docile. As Miss Cunegonde had a great disposition for the sciences, she breathlessly observed the repeated experiments of which she was a witness; she clearly perceived [Pg 4]the force of the Doctor's reasons, the effects, and the causes; she turned back greatly flurried, quite pensive, and filled with the desire to be learned; dreaming that she might well be a sufficient reason for young Candide, and he for her.
-
-                    She met Candide on reaching the castle and blushed; Candide blushed also; she wished him good morrow in a faltering tone, and Candide spoke to her without knowing what he said. The next day after dinner, as they went from table, Cunegonde and Candide found themselves behind a screen; Cunegonde let fall her handkerchief, Candide picked it up, she took him innocently by the hand, the youth as innocently kissed the young lady's hand with particular vivacity, sensibility, and grace; their lips met, their eyes sparkled, their knees trembled, their hands strayed. Baron Thunder-ten-Tronckh passed near the screen and beholding this cause and effect chased Candide from the castle with great kicks on the backside; Cunegonde fainted away; she was boxed on the ears by the Baroness, as soon as she came to herself; and all was consternation in this most magnificent and most agreeable of all possible castles.[Pg 5]
-
-
-
                 </p>
             </div>
         </div>
