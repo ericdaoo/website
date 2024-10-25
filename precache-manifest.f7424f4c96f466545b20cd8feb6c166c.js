55831be16e19f419b5acf8fe1c1c59d17b16636c
ericdaoo <53070250+ericdaoo@users.noreply.github.com>
2024-10-24 21:40:06 -0400

    Updates

---
+++ precache-manifest.f7424f4c96f466545b20cd8feb6c166c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3f8403bec5fd9074b46d2eaa6448be",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "48bb7a41d0aa22675922",
+    "url": "/website/static/css/main.31585f25.chunk.css"
+  },
+  {
+    "revision": "6910f25591d50674fc82",
+    "url": "/website/static/js/2.92fcd8ad.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/website/static/js/2.92fcd8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48bb7a41d0aa22675922",
+    "url": "/website/static/js/main.94b6af8d.chunk.js"
+  },
+  {
+    "revision": "7eb8aca0ab083c9a4056",
+    "url": "/website/static/js/runtime-main.08ac27eb.js"
+  },
+  {
+    "revision": "fc3cd6b3ab3b5f701a5a0347508fbd0e",
+    "url": "/website/static/media/HanyiSentyBubbleTea Regular.fc3cd6b3.ttf"
+  }
+]);
